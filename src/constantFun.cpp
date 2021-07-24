8a895a0b68ba5072fc8f739a84d295b1601e173e
Emmanuel M. Achana <achanamosey@gmail.com>
2021-07-24 08:35:25 +0000

    Updated the code. Added more

---
+++ src/constantFun.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    const double MY_PI = 3.14159;
+    const string MY_NAME = "Emmanuel M. Achana";
+
+    cout << MY_NAME << endl;
+
+
+    return 0;
+}
