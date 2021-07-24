9023a7dd90abe3821142772812d0b0ba8c12565c
Emmanuel M. Achana <achanamosey@gmail.com>
2021-07-24 07:35:06 +0000

    New cpp file to test arithmetics.

---
+++ src/arithmetics.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Arithmetic Operators
+    int a = 10;
+    int b = 3;
+    int sum = a + b;
+    int difference = a - b;
+    int product = a * b;
+    int quotient = a / b;
+    int remainder = a % b;
+
+    cout << sum << endl;
+    cout << difference << endl;
+    cout << product << endl;
+    cout << quotient << endl;
+    cout << remainder << endl;
+    return 0;
+}
