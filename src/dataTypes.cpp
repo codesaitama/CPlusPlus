68b5f10f986aa4cf65e911c6d7a799b086e6f856
Emmanuel M. Achana <achanamosey@gmail.com>
2021-07-24 07:30:49 +0000

    Created a new file to look at data types.

---
+++ src/dataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Data types
+    int myInt = 15;
+    double myDouble = 3.14159;
+    char singleChar = 'a';
+    string myName = "Emmanuel Achana";
+    bool isRaining = false;
+
+    cout << myInt << endl;
+    cout << myDouble << endl;
+    cout << singleChar << endl;
+    cout << "Hello " << myName << endl;
+    cout << isRaining << endl;
+    cout << boolalpha << isRaining << endl;
+}
