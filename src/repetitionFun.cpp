c394ee9fb6827c46dbdc2c54f3b1f6df8a0c1ffe
Emmanuel Achana <achanamosey@gmail.com>
2021-08-02 13:16:56 +0000

    Added a repetition test.

---
+++ src/repetitionFun.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int count = 0;
+
+    while(count < 10){
+        cout << count << endl;
+        count += 1;
+    }
+
+    return 0;
+}
