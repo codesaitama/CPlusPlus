ad0c8b55056bb620f0adbcd1cf7a1b711c6a350b
Emmanuel Achana <achanamosey@gmail.com>
2021-08-02 14:30:28 +0000

    Simple program to sum up user input.

---
+++ src/sumFun.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int sum = 0;
+
+    int input;
+    cout << "Please enter a non-negative integer to continue, "<< endl;
+    cout << "or negative if you want to quit and get the total" << endl;
+    cin >> input;
+
+    while (input >= 0)
+    {
+        sum = sum + input;
+        cout << "Please enter a non-negative integer to continue, "<< endl;
+        cout << "or negative if you want to quit and get the total" << endl;
+        cin >> input;
+    }
+
+    cout << "The total sum of number is " << sum << endl;
+    
+
+    return 0;
+}
