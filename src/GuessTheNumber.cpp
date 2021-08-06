9c2a67e043aec036489bf8e173f6d3aeb2bafebe
Emmanuel Achana <achanamosey@gmail.com>
2021-08-06 14:57:28 +0000

    Quickly updated the GuessTheNumber program.

--- src/GuessTheNumber.cpp
+++ src/GuessTheNumber.cpp
@@ -9,19 +9,23 @@ int main(){
     srand(time(nullptr));
 
     int numberOfTries = 0;
-    int guess;
+    int guess, allowedNumberOfGuesses = 3;
+    int randomGuess = rand() % 100 + 1;
+    cout << randomGuess << endl;
 
-    while(numberOfTries < 3){
-        int randomGuess = rand() % 100 + 1;
-
-        //cout << randomGuess << endl;
+    while(numberOfTries < allowedNumberOfGuesses){
 
         numberOfTries++;
         cout << "Guess a number between 0 and 101 " << endl;
         cin >> guess;
 
+        if(guess > 100 || guess < 1){
+            cout << "That was a waste of guess. You must pick a number between 1 and 100 inclusive!" << endl;
+            continue;
+        }
+
         if(randomGuess == guess){
-            cout << "Congratulations! You guessed the number in " << numberOfTries << "! Thnaks for playing" << endl;
+            cout << "Congratulations! You guessed the number in " << numberOfTries << " tries! Thnaks for playing" << endl;
             break;
         }
 
