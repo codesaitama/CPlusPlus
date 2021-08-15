42465383361da22d998665274e8134e8c53891c7
Emmanuel M. Achana <achanamosey@gmail.com>
2021-08-15 23:00:19 +0000

    Minor modification

--- src/ArrayFunTest.cpp
+++ src/ArrayFunTest.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main(){
 
-    array<int, 5> myIntArray {0, 0, 0, 0, 0};
+    // array<int, 5> myIntArray {0, 0, 0, 0, 0};
+    array<int, 5> myIntArray {};
 
     for(int a : myIntArray){
         cout << a << endl;
